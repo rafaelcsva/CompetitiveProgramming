9de4fe63f138b436ad86bd18b8158c19697c933a
rafaelcastr141 <rafaelgyncastro63@gmail.com>
2017-10-15 10:17:42 -0200

    Accepted solution from C

---
+++ exercicios/CodeForces/440/AfterContest/c.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+
+#define	ll	long long
+
+using namespace std;
+
+int main (){
+	int q;
+	
+	cin>>q;
+
+	while(q--){
+		ll n;
+
+		cin>>n;
+
+		ll q = n / 2;
+		ll r = n % 2;
+		int resp = 0;
+		
+		if(r != 0){
+			if(q >= 4){
+				q -= 4;
+				resp++;
+			}
+			else{
+				resp = -1;
+			}
+		}
+
+		if(resp != -1 && q % 2 != 0){
+			if(q >= 3){
+				q -= 3;
+				resp++;
+			}else{
+				resp = -1;
+			}
+		}
+
+		if(resp == -1 || resp + q / 2 <= 0){
+			resp = -1;
+		}else{
+			resp = resp + q/2;
+		}
+
+		cout<<resp<<endl;
+
+	}
+
+	return 0;
+}
